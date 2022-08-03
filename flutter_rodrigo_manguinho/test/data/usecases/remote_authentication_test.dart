0108695e3fde84cfddaa93ddbbe49c08a44a3c3e
Larissa dos Santos Nunes - LNN <larissa.nunes@iteris.com.br>
2022-08-03 14:28:21 -0300

    fix: trocando mockito por mocktail

--- flutter_rodrigo_manguinho/test/data/usecases/remote_authentication_test.dart
+++ flutter_rodrigo_manguinho/test/data/usecases/remote_authentication_test.dart
@@ -1,6 +1,6 @@
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
 
 class RemoteAuthentication {
   final HttpClient httpClient;
